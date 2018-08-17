63e464d5a9a7033e28855365a2aa9e259b165ec5
Stuart McLaren <stuart.mclaren@hpe.com>
2018-08-17 14:44:43 +0100

    Add example services which propagate headers between them.
    For reference purposes.

---
+++ example/server2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-contrib/opengintracing"
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/zipkin"
+	"net/http"
+)
+
+func main() {
+	// Configure tracing
+	propagator := zipkin.NewZipkinB3HTTPHeaderPropagator()
+	trace, closer := jaeger.NewTracer(
+		"api_gateway",
+		jaeger.NewConstSampler(true),
+		jaeger.NewNullReporter(),
+		jaeger.TracerOptions.Injector(opentracing.HTTPHeaders, propagator),
+		jaeger.TracerOptions.Extractor(opentracing.HTTPHeaders, propagator),
+		jaeger.TracerOptions.ZipkinSharedRPCSpan(true),
+	)
+	defer closer.Close()
+	opentracing.SetGlobalTracer(trace)
+	var fn opengintracing.ParentSpanReferenceFunc
+	fn = func(sc opentracing.SpanContext) opentracing.StartSpanOption {
+		return opentracing.ChildOf(sc)
+	}
+
+	// Set up routes
+	r := gin.Default()
+	r.POST("",
+        opengintracing.SpanFromHeadersHttpFmt(trace, "service2", fn, false),
+		handler)
+	r.Run(":8002")
+}
+
+func handler(c *gin.Context) {
+	_, found := opengintracing.GetSpan(c)
+	if found == false {
+		fmt.Println("Span not found")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Incoming Headers")
+	for k, v := range c.Request.Header {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+	c.Status(http.StatusOK)
+}
